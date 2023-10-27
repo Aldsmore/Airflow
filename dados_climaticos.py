aa6cc3fa37b1021328fcb6d88cd5f0a91d4322f8
Aldsmore <andre1991.ml@gmail.com>
2023-10-27 19:47:46 -0300

    Add files via upload

---
+++ dados_climaticos.py
@@ -0,0 +1,48 @@
+import pendulum
+import os
+import pandas as pd
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.macros import ds_add
+from os.path import join
+from datetime import datetime, timedelta
+
+with DAG(
+    "dados_climaticos_rj",
+    start_date=pendulum.datetime(2023, 9, 24, tz="UTC"),
+    schedule_interval='0 0 * * 0', #domingo
+) as dag:
+    
+    tarefa_1 = BashOperator(
+        task_id = 'cria_pasta',
+        bash_command = 'mkdir -p "/home/aldsmore/Documents/airflow/semana={{data_interval_end.strftime("%Y-%m-%d")}}"'
+    )
+
+    def extrai_dados(data_interval_end):
+        key_path = '/home/aldsmore/Documents/airflow/dags/key.txt'
+
+        with open(key_path, 'r') as key_file:
+            key = key_file.read().strip()
+        key_file.close()
+
+        city = 'RiodeJaneiro'
+
+        URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+        f'{city}/{data_interval_end}/{ds_add(data_interval_end, 7)}?unitGroup=metric&include=days&key={key}&contentType=csv')
+
+        dados = pd.read_csv(URL)
+
+        file_path = f'/home/aldsmore/Documents/airflow/semana={data_interval_end}/'
+
+        dados.to_csv(file_path + 'dados_brutos.csv')
+        dados[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
+        dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
+
+    tarefa_2 = PythonOperator(
+        task_id = 'extrai_dados',
+        python_callable = extrai_dados,
+        op_kwargs = {'data_interval_end': '{{data_interval_end.strftime("%Y-%m-%d")}}'}
+    )
+
+    tarefa_1 >> tarefa_2
